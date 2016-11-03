17c8fae20e16a00e00c3fda71775315e068a00a8
nickkolok <nickkolok@mail.ru>
2016-11-03 10:29:24 +0300

    Функция генерации ключа и сгенерированный ключ

---
+++ generate-key.js
@@ -0,0 +1,8 @@
+require('./ext/Object_generic.js');
+require('./ext/String_generic.js');
+require('./ext/array.js');
+require('./ext/string.js');
+require('./ext/number_math.js');
+
+var alphabet='йцукенгшщзхъфывапролджэячсмитьбю';
+console.log(alphabet.mesh());
